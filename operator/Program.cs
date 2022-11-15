a6399f8b82e6c56626a85e6dd508f2ed4f3b0a58
da <mikaelson4ik@gmail.com>
2022-11-15 19:01:32 +0300

    operator

---
+++ operator/Program.cs
@@ -0,0 +1,21 @@
+﻿string name;
+char symbol;
+int nameLenght;
+Console.WriteLine("Введите имя:");
+name = Console.ReadLine();
+nameLenght = name.Length;
+Console.Write("Введите символ: ");
+symbol = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("\n");
+
+for (int i = 0; i < nameLenght + 2; i++)
+{
+    Console.Write(symbol);
+}
+Console.WriteLine("\n" + symbol + name + symbol);
+
+for (int i = 0; i < nameLenght + 2; i++)
+{
+    Console.Write(symbol);
+}
+Console.ReadKey();
