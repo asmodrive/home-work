833c61a2043b4cae9cbc7749ee900ee6cec3fed7
da <mikaelson4ik@gmail.com>
2022-11-21 20:44:18 +0300

    degree

---
+++ stepen/Program.cs
@@ -0,0 +1,17 @@
+﻿int minBoarder = 2;
+int maxBoarder = 30000;
+int randomSeed = 1938;
+int degree = 0;
+int baseNumber = 2;
+var baseInDegree = MathF.Pow(baseNumber, degree);
+Random random = new Random(randomSeed);
+var number = random.Next(minBoarder, maxBoarder);
+Console.WriteLine(number);
+
+while (number > baseInDegree)
+{
+   degree++;
+   baseInDegree = MathF.Pow(baseNumber, degree);
+}
+
+Console.WriteLine($"Число = {number},\nстепень основного числа {baseNumber} = {degree}, \nосновное число в степени = {baseInDegree}.");
