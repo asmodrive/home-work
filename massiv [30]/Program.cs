5e33da9d2e284d85998259a141d70bd879b06382
da <mikaelson4ik@gmail.com>
2022-12-02 14:37:43 +0300

    length

--- massiv [30]/Program.cs
+++ massiv [30]/Program.cs
@@ -1,16 +1,32 @@
-﻿int[] numbers = new int[30];
+﻿int arrayLength = 30;
+int[] numbers = new int[arrayLength];
 int minNumber = 0;
 int maxNumber = 30;
 Random random = new Random();
-int maxValue = numbers[0];
+
+Console.WriteLine("Исходный массив.");
 
 for (int i = 1; i < numbers.Length; i++)
 {
     numbers[i] = random.Next(minNumber, maxNumber);
+    Console.Write($"{numbers[i]} ");
+}
 
-    if (numbers[i] > maxValue)
+Console.WriteLine("\nЛокальные максимумы.");
+if (numbers[0] > numbers[1] )
+{
+    Console.Write($"\n{ numbers[0]}");
+}
+
+for (int i = 1; i < numbers.Length-1; i++)
+{
+   if (numbers[i] > numbers[i-1] && numbers[i] > numbers[i+1])
     {
-        maxValue = numbers[i];
+        Console.Write($"{numbers[i]} ");
     }
 }
-Console.WriteLine(maxValue);
+
+if (numbers[numbers.Length-1] > numbers[numbers.Length-2])
+{
+    Console.Write($"{numbers[numbers.Length-1]}");
+}
