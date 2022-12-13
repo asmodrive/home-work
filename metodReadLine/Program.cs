a7129a56d864266a90aeb83d6412ad237b154981
da <mikaelson4ik@gmail.com>
2022-12-14 00:42:21 +0300

    readkey

--- metodReadLine/Program.cs
+++ metodReadLine/Program.cs
@@ -4,23 +4,24 @@ Console.WriteLine($"Вы ввели: {Enter()}");
 
 static int Enter()
 {
-    bool isRunning = false;
+    bool isRunning = true;
     int value = 0;
 
-    while (isRunning == false)
+    while (isRunning)
     {
         Console.WriteLine("Введите число:");
         string userInput = Console.ReadLine();
 
-        if (int.TryParse(userInput, out value) == true)
+        if (int.TryParse(userInput, out value))
         {
             Console.WriteLine($"{value} - это число");
-            isRunning = true;
+            isRunning = false;
         }
         else
         {
             Console.WriteLine($"{userInput} - это не число.");
         }
     }
+
     return value;
 }
