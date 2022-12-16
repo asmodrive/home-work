01ed57bafc7aef720fc7b49f78eea7708db5a017
da <mikaelson4ik@gmail.com>
2022-12-16 17:47:25 +0300

    shuffle

--- Shuffle/Program.cs
+++ Shuffle/Program.cs
@@ -1,14 +1,40 @@
-﻿static void Shuffle(int[] numbers)
+﻿using System;
+int arraySize = 10;
+int[] numbers = new int[arraySize];
+int minValue = 0;
+int maxValue = 5;
+Random random = new Random(123412);
+
+for (int i = 0; i < numbers.Length; i++)
 {
-    Random rand = new Random();
+    numbers[i] = random.Next(minValue, maxValue);
+}
 
-    for (int i = numbers.Length - 1; i >= 1; i--)
+PrintNumbers(numbers);
+Console.WriteLine();
+numbers = Shuffle(numbers);
+Console.WriteLine();
+PrintNumbers(numbers);
+
+static int[] Shuffle(int[] numbers)
+{
+    Random random = new Random(123412);
+
+    for (int i = numbers.Length-1; i >= 1; i--)
     {
-        int j = rand.Next(i + 1);
-
-        int tmp = numbers[j];
+        int j = random.Next(i + 1);
+        int temporaryNumber = numbers[j];
         numbers[j] = numbers[i];
-        numbers[i] = tmp;
-        Console.WriteLine(tmp);
+        numbers[i] = temporaryNumber;
+    }
+
+    return numbers;
+}
+
+static void PrintNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
     }
 }
