32ae1796d05c47b235204aeda84cecadeb43d294
da <mikaelson4ik@gmail.com>
2023-01-19 22:17:52 +0300

    symaqueue

---
+++ queue/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumMoney = 0;
+            Queue<int> money = new Queue<int>();
+
+            money.Enqueue(300);
+            money.Enqueue(2690);
+            money.Enqueue(150);
+            money.Enqueue(45);
+            money.Enqueue(530);
+            money.Enqueue(250);
+
+
+            while (money.Count > 0)
+            {
+                Console.WriteLine($"Текущий баланс счета: {sumMoney += money.Dequeue()}");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
