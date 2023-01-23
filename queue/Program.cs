6034bb18ddc5d59610f6d5163e4619597c1ce34c
da <mikaelson4ik@gmail.com>
2023-01-23 18:54:41 +0300

    sumakorzini

--- queue/Program.cs
+++ queue/Program.cs
@@ -20,9 +20,9 @@ namespace queue
             money.Enqueue(530);
             money.Enqueue(250);
 
-
             while (money.Count > 0)
             {
+                Console.WriteLine($"Сумма корзины клиента: {money.Peek()}");
                 sumMoney += money.Dequeue();
                 Console.WriteLine($"Текущий баланс счета: {sumMoney}");
                 Console.ReadKey();
