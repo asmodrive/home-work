38622f40cda72fea6aadd78d30df73b31a2c4d31
da <mikaelson4ik@gmail.com>
2023-01-23 20:00:38 +0300

    word

--- slovo/Program.cs
+++ slovo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,26 @@ namespace slovo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите слово:");
+           Dictionary<string, string> words = new Dictionary<string, string>();
+
+            words.Add("Собака", "Такое слово есть.");
+            words.Add("Пиво", "Такое слово есть.");
+            words.Add("Дорога", "Такое слово есть.");
+            words.Add("Дом", "Такое слово есть.");
+            words.Add("Парк", "Такое слово есть.");
             string userInput = Console.ReadLine();
-            Console.WriteLine("Такого слова нет.");
+
+            if (words.ContainsKey(userInput))
+            {
+                Console.WriteLine(words[userInput]);
+            }
+            else
+            {
+                Console.WriteLine("Такого слова нет.");
+            }
         }
+            
     }
+
+    
 }
