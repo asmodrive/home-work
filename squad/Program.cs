bbb5eca0054a6ed9afaa7b06b5ae23cea3b79a77
da <mikaelson4ik@gmail.com>
2023-04-24 16:32:49 +0300

    even

--- squad/Program.cs
+++ squad/Program.cs
@@ -34,39 +34,24 @@ namespace squad
 
     class War
     {
-        private Platoon _firstPlatoon = new Platoon(new List<Soldier>
-        {
-                new Soldier("Сэм", 5, 100, 20),
-                new Soldier("Майкл", 15, 100, 20),
-                new Soldier("Джери", 10, 100, 20),
-                new Soldier("Том", 25, 100, 20),
-                new Soldier("Уолтер", 40, 100, 20),
-                new Soldier("Джейсон", 20, 100, 20)
-        }
-        );
-
-        private Platoon _secondPlatoon = new Platoon(new List<Soldier>
-            {
-               new Soldier("Робин", 7, 100, 20),
-                new Soldier("Гари", 60, 100, 20),
-                new Soldier("Алекс", 30, 100, 20),
-                new Soldier("Джордж", 10, 100, 20),
-                new Soldier("Уильям", 32, 100, 20),
-                new Soldier("Дуглас", 17, 100, 20)
-            }
-        );
+        private Platoon _firstPlatoon;
+        private Platoon _secondPlatoon;
 
         public void StartBattle()
         {
+            _firstPlatoon = new Platoon(true);
+            _secondPlatoon = new Platoon(false);
+
             while (_firstPlatoon.GetCountSoldiers() && _secondPlatoon.GetCountSoldiers())
             {
-                _firstPlatoon.ShowPlatoon();
-                var firstSoldier = _firstPlatoon.GetSquadSoldier();
-                var secondSoldier = _secondPlatoon.GetSquadSoldier();
+                _firstPlatoon.ShowInfo();
+                _secondPlatoon.ShowInfo();
+                var firstSoldier = _firstPlatoon.GetSoldier();
+                var secondSoldier = _secondPlatoon.GetSoldier();
                 firstSoldier.UseSkillAttack();
                 secondSoldier.UseSkillDefence();
-                firstSoldier.GiveAttack(secondSoldier);
-                secondSoldier.GiveAttack(firstSoldier);
+                firstSoldier.Attack(secondSoldier);
+                secondSoldier.Attack(firstSoldier);
                 _firstPlatoon.RemoveDeadSoldiers();
                 _secondPlatoon.RemoveDeadSoldiers();
                 Console.ReadKey();
@@ -74,25 +59,45 @@ namespace squad
 
             Console.WriteLine("Битва окончена!");
         }
-
-        public void ShowPlatoon(List<Soldier> soldiers)
-        {
-            foreach (var soldier in soldiers)
-            {
-                soldier.ShowInfo();
-            }
-
-            Console.WriteLine();
-        }
     }
 
     class Platoon
     {
         private List<Soldier> _soldiers = new List<Soldier>();
+        private Random _random = new Random();
 
-        public Platoon(List<Soldier> soldiers)
+        public Platoon(bool even)
         {
-            _soldiers = soldiers;
+            var soldiers = ListFighters();
+
+            for (int i = 0; i < soldiers.Count; i++)
+            {
+                if (i % 2 == 0 && even || i % 2 != 0 && even == false)
+                {
+                    _soldiers.Add(soldiers[i]);
+                }
+            }
+        }
+
+        private List<Soldier> ListFighters()
+        {
+            var soldiers = new List<Soldier>()
+        {
+                new Soldier("Сэм", 5, 100, 20),
+                new Soldier("Майкл", 15, 100, 20),
+                new Soldier("Джери", 10, 100, 20),
+                new Soldier("Том", 25, 100, 20),
+                new Soldier("Уолтер", 40, 100, 20),
+                new Soldier("Джейсон", 20, 100, 20),
+                new Soldier("Робин", 7, 100, 20),
+                new Soldier("Гари", 60, 100, 20),
+                new Soldier("Алекс", 30, 100, 20),
+                new Soldier("Джордж", 10, 100, 20),
+                new Soldier("Уильям", 32, 100, 20),
+                new Soldier("Дуглас", 17, 100, 20)
+        };
+
+            return soldiers;
         }
 
         public bool GetCountSoldiers()
@@ -112,7 +117,7 @@ namespace squad
             }
         }
 
-        public void ShowPlatoon()
+        public void ShowInfo()
         {
             foreach (var soldier in _soldiers)
             {
@@ -122,7 +127,7 @@ namespace squad
             Console.WriteLine();
         }
 
-        public Soldier GetSquadSoldier()
+        public Soldier GetSoldier()
         {
             Random random = new Random();
 
@@ -135,42 +140,41 @@ namespace squad
         public Soldier(string name, int attack, int healph, int armor)
         {
             Name = name;
-            Attack = attack;
+            Damage = attack;
             Healph = healph;
             Armor = armor;
         }
 
         public string Name { get; private set; }
-        public int Attack { get; protected set; }
+        public int Damage { get; protected set; }
         public int Healph { get; protected set; }
         public int Armor { get; protected set; }
 
         public void ShowInfo()
         {
-            Console.WriteLine($"Имя бойца: {Name}, атака: {Attack}, здоровье: {Healph}, броня: {Armor}.");
+            Console.WriteLine($"Имя бойца: {Name}, атака: {Damage}, здоровье: {Healph}, броня: {Armor}.");
         }
 
         public void TakeDamage(int damage)
         {
-            if (damage < Armor)
+            if (damage > Armor)
             {
-                Armor = damage;
+                Healph -= damage - Armor;
             }
 
-            Healph -= damage - Armor;
             Console.WriteLine($"{Name} получил {damage}, осталось {Healph} и {Armor}.");
         }
 
-        public void GiveAttack(Soldier soldier)
+        public void Attack(Soldier soldier)
         {
-            soldier.TakeDamage(Attack);
+            soldier.TakeDamage(Damage);
         }
 
         public void UseSkillAttack()
         {
             int damageBuff = 5;
 
-            Attack *= damageBuff;
+            Damage *= damageBuff;
         }
 
         public void UseSkillDefence()
